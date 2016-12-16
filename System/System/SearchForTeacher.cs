b6980c2153ddb5af1d81d18413c053515dc841d2
Vítor Bapitsta <2054313@student.uma.pt>
2016-12-16 16:47:23 +0000

    IFilterHandle Interface and Users Classes

---
+++ System/System/SearchForTeacher.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public class SearchForTeacher : IFilterHandler
+    {
+        public void Filter(string aName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

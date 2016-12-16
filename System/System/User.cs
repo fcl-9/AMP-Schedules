7b8daedf57dee0860c69fad390be0162d7929eee
Vítor Bapitsta <2054313@student.uma.pt>
2016-12-16 17:00:22 +0000

    Add relation to User

--- System/System/User.cs
+++ System/System/User.cs
@@ -1,8 +1,11 @@
-﻿namespace System
+﻿using System.Collections.Generic;
+
+namespace System
 {
     public abstract class User
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public ICollection<Course> Courses { get; set; }
     }
 }

9541f6b9d0d8a7d31cde988e477bd0ec11c13abe
Fábio Luís <fabiocl93@gmail.com>
2016-12-16 17:10:44 +0000

    Base System Architecture

--- System/System/ISubject.cs
+++ System/System/ISubject.cs
@@ -1,7 +1,13 @@
-﻿namespace System
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace System
 {
     public interface ISubject:IDecorator
     {
-        
+        IEnumerator<IObserver> Observers { get; set; }
+        void Add(IObserver observer);
+        void Remove(IObserver observer);
+        void Notify();
     }
 }
--- System/System/WeeklyView.cs
+++ System/System/WeeklyView.cs
@@ -1,7 +1,17 @@
-﻿namespace System
+﻿using System.Collections.Generic;
+
+namespace System
 {
     public class WeeklyView:IViewHandler
     {
-        
+        public IEnumerator<ISubject> Existingsubjects { get; set; }
+
+        public void RenderView(int viewType)
+        {
+        }
+
+        public void Update()
+        {
+        }
     }
 }

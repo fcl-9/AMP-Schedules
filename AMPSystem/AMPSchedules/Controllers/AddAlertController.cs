d628001d4568e6f399a42fadb591df504bb712f0
filipaNobrega <filipanobrega1994@gmail.com>
2016-12-29 12:19:53 +0000

    Database queries (add user, remove alert and return item if exists)

---
+++ AMPSystem/AMPSchedules/Controllers/AddAlertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
+{
+    public class AddAlertController : Controller
+    {
+        // GET: AddAlert
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

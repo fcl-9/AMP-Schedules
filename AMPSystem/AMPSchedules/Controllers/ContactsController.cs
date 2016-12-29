9118cd879d53fc6b372c228027f31e01a8b386b3
Fábio Luís <fabiocl93@gmail.com>
2016-12-29 09:10:09 +0000

    Contacts

--- AMPSystem/AMPSchedules/Controllers/ContactsController.cs
+++ AMPSystem/AMPSchedules/Controllers/ContactsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AMPSystem.Classes;
+using AMPSystem.Classes.LoadData;
 using AMPSystem.Interfaces;
 using Newtonsoft.Json;
 
@@ -26,13 +27,29 @@ namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
             loadData.GetCourses();
             loadData.GetRooms();
             loadData.GetTeachers();
+            loadData.GetUserCourses("2054313");
+            loadData.GetSchedule("2054313");
 
-            TimeTableManager manager = new TimeTableManager(loadData);
-            foreach (var teacher in manager.Repository.Teachers)
+            var startDateTime = Convert.ToDateTime(Request.QueryString["start"]);
+            var endDateTime = Convert.ToDateTime(Request.QueryString["end"]);
+            
+            //The manager will start the timetableitem list with the data read from the repo
+            TimeTableManager manager = new TimeTableManager(loadData, startDateTime, endDateTime);
+
+            ICollection<ITimeTableItem> items = new List<ITimeTableItem>();
+
+            foreach (var officeHour in manager.TimeTable.ItemList)
             {
-                Debug.Write(teacher);
+                Debug.Write(officeHour);
+                //if (officeHour == typeof(OfficeHours))
+                //{
+                    //officeHour.Teacher
+                    //officeHour.Name;
+                    //officeHour.Rooms;
+                    items.Add(officeHour);
+                //}
             }
-            return Content(JsonConvert.SerializeObject(manager.Repository.Teachers.ToArray()), "application/json");
+            return Content(JsonConvert.SerializeObject(items.ToArray()), "application/json");
         }
     }
 }

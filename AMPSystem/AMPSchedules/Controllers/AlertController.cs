092862bf07847dca9e86da3c887b9d43374352d2
filipaNobrega <filipanobrega1994@gmail.com>
2016-12-30 14:37:03 +0000

    Alert Controller sending alerts

--- AMPSystem/AMPSchedules/Controllers/AlertController.cs
+++ AMPSystem/AMPSchedules/Controllers/AlertController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using AMPSystem.Classes;
 using Microsoft.Graph;
+using Newtonsoft.Json;
 using Resources;
+using AMPSystem.Interfaces;
 
 namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
 {
@@ -30,7 +33,21 @@ namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
 
         public override ActionResult Hook(TimeTableManager manager)
         {
-            return base.Hook(manager);
+            Debug.WriteLine("Message: " + Request.QueryString);
+
+            foreach (var key in Request.QueryString)
+            {
+                Debug.WriteLine(Request.QueryString[(string) key]);
+            }
+
+            var item = ((List<ITimeTableItem>)manager.TimeTable.ItemList).Find(
+                i =>
+                    i.Name == Request.QueryString["name"] &&
+                    i.StartTime == Convert.ToDateTime(Request.QueryString["startTime"]) &&
+                    i.EndTime == Convert.ToDateTime(Request.QueryString["endTime"]));
+
+            var alerts = item.Alerts.ToList();
+            return Content(JsonConvert.SerializeObject(alerts.ToArray(), new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }), "application/json");
         }
     }
 }

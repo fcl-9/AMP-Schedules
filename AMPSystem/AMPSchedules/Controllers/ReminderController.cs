fb0d900304994829c26d7ced34107b60e725ba07
Vítor Bapitsta <2054313@student.uma.pt>
2017-01-17 14:31:40 +0000

    Add Reminder controller

---
+++ AMPSystem/AMPSchedules/Controllers/ReminderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AMPSystem.Classes;
+using AMPSystem.Interfaces;
+
+namespace AMPSchedules.Controllers
+{
+    public class ReminderController : Controller
+    {
+        public ActionResult Index()
+        {
+            var item = ((List<ITimeTableItem>)TimeTableManager.Instance.TimeTable.ItemList).Find(
+                i =>
+                    i.Name == Request.QueryString["name"] &&
+                    i.StartTime == Convert.ToDateTime(Request.QueryString["startTime"]));
+            return Content(item.Reminder);
+        }
+        public void Add()
+        {
+            var reminder = Request.QueryString["reminder"];
+        }
+
+        public void Remove()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
